0cd5040ea3c35f75af2b8ca7c917a56cd8420495
longbiao_wu <615944323@qq.com>
2025-03-28 19:22:35 +0800

    feat: add tailwindcss support

--- config/index.ts
+++ config/index.ts
@@ -36,12 +36,24 @@ export default defineConfig<'vite'>(async (merge, { command, mode }) => {
 
           },
         },
+        // 添加 tailwindcss 配置
+        tailwindcss: {
+          enable: true,
+        },
+        autoprefixer: {
+          enable: true,
+        },
         cssModules: {
           enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
           config: {
             namingPattern: 'module', // 转换模式，取值为 global/module
             generateScopedName: '[name]__[local]___[hash:base64:5]',
           },
+        },
+      },
+      sassLoaderOption: {
+        sassOptions: {
+          silenceDeprecations: ['legacy-js-api', 'import'],
         },
       },
     },
